353fae2c0a090672e6373c1f21852f796065687f
zhoujinhai <932853432@qq.com>
2023-07-26 14:22:32 +0800

    Update hourglass.h

--- inference/hourglass.h
+++ inference/hourglass.h
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector> 
-#include "ncnn/net.h"
 
 class KPOption
 {
@@ -9,7 +8,6 @@ public:
 	const int targetSize = 256; 
 	const float MEANS[3] = { 123.675, 116.28, 103.53 };
 	const float STD[3] = { 1.0 / 58.395, 1.0 / 57.120, 1.0 / 57.375 }; 
-	int numKp = 3;
 };
 
 class Point
@@ -19,6 +17,47 @@ public:
 	Point(float px = 0.0, float py = 0.0) : x(px), y(py) {}
 };
 
+class KeyPointCV
+{
+public:
+
+	KeyPointCV();
+	~KeyPointCV();
+
+	/*
+	* @brief: load the keypoint model that has been conveted to onnx format
+	*
+	* @param[in] modelPath: the file path of model
+	* @param[in] opt: some params for yolact net
+	*
+	*/
+	bool Build(std::string modelPath, const KPOption& opt);
+
+	/*
+	* @brief: judge the model's whether load Ok
+	*/
+	bool IsValid() const;
+
+	void Clear();
+
+	/*
+	* @brief: predict the img
+	*
+	* @param[in] imgPath: the file path of image
+	* @param[out] keypoints: the predict result
+	* @param[in] opt: some params for predict and network
+	*
+	*/
+	bool Predict(std::string imgPath, std::vector<Point>& keypoints, const KPOption& opt);
+
+	/*
+	* @brief: show the predict res
+	*/
+	void Show(std::string imgPath, const std::vector<Point>& keypoints, const KPOption& opt);
+
+private:
+	void* net_;
+};
  
 class KeyPoint
 {
@@ -28,7 +67,7 @@ public:
 	~KeyPoint();
 
 	/*
-	* @brief: load the keypoint model that has been conveted to onnx format
+	* @brief: load the keypoint model that has been conveted to ncnn format
 	*
 	* @param[in] modelPath: the file path of model
 	* @param[in] opt: some params for yolact net
@@ -60,5 +99,5 @@ public:
 
 private:
 	struct CPrivate;
-	CPrivate* const pD_;  
+	CPrivate* pD_;  
 };
